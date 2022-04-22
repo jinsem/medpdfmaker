f9c5e7b48aa9cf66cb2629c7a5680db5293d6883
Jin <jinsem@gmail.com>
2022-04-21 21:18:55 -0700

    Add shell scrip to run utility

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Dapp.properties.ext=/home/jin/prj/medpdfmaker/localwork/med-pdf-maker.properties -jar ./target/med-pdf-maker.jar /home/jin/prj/medpdfmaker/localwork/DECEMBER\ 2020\ FOR\ ACROBAT-Date-issue.xlsx 
